edcb14f19411c6cee49fdda9a1115a7d48d51b3c
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-24 15:36:05 -0300

    Added react-router package Created another 2 pages.

---
+++ webApp/desterro-web-app/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <React.Fragment>
+        <h1>This is my about Page!</h1>
+        <p>For now, it´s just this.</p>
+    </React.Fragment>
+
+)
+
+export default AboutPage;
---
+++ webApp/desterro-web-app/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+    <h1>Welcome to Desterro Rugby Infantil System</h1>
+    <p> This is the web app frontend with React</p>
+    </React.Fragment>
+    
+);
+
+export default HomePage;
